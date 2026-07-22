cdc8b2627175d5c85392b44fcc10b6f3cc9209db
katalyst-brd <maxortega273@gmail.com>
2026-07-22 15:08:47 -0600

    made timer display on main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,18 @@
 extends Node
+@onready var timer: Timer = $Timer
+@onready var timeLabel: Label = $Label
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
  
-func StartBomb() -> void:
-	var NewTimerNode = Timer.new()
+func startBomb() -> void:
+	timer.timeout.connect(gameOver)
 	
+func gameOver() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	timeLabel.text = str(int(timer.time_left))
