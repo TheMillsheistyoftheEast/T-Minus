35030a472a4cd4f13c5559af6ddf5e303494bb55
TheMillsheistyoftheEast <joshualucasmiller301@gmail.com>
2026-07-23 15:06:48 -0600

    changed wires game and pictures in assets

--- scripts/cut_the_wires_game.gd
+++ scripts/cut_the_wires_game.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+#create signal names
+signal cut_wire_blue
+signal cut_wire_red
+signal cut_wire_yellow
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +19,25 @@ func _process(delta: float) -> void:
 	#If wire is incorrect then do a thing
 	pass
 	
+#what to do when button is pressed
+func _on_blue_texture_button_pressed() -> void:
+	cut_wire_blue.emit()
+	
+func _on_red_texture_button_pressed() -> void:
+	cut_wire_red.emit()
+
+func _on_yellow_texture_button_pressed() -> void:
+	cut_wire_yellow.emit()
+
+#what happens when signal is recieved
+func _on_cut_wire_red() -> void:
+	print("red wire is cut")
+	#shut off bomb
+	
+func _on_cut_wire_blue() -> void:
+	print("blue wire is cut")
+	#make timer go down faster
+	
+func _on_cut_wire_yellow() -> void:
+	print("yellow wire is cut")
+	get_tree().quit()
